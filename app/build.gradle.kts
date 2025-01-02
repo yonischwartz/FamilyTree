d040a7822a0e2e146c562b646d02b4391e851a59
Yoni Schwartz <yschwartz23@gmail.com>
2025-01-02 18:36:47 +0200

    sync worked

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,10 @@ android {
 dependencies {
     implementation(libs.composeUi)
     implementation(libs.composeMaterial)
-    implementation(platform("com.google.firebase:firebase-bom:32.1.1"))
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.material3:material3:1.3.1")
 }
 
